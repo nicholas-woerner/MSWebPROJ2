044fe3e3d6e68ff06bdd82e624b61569e6fe2c23
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-10-15 14:23:39 -0500

    worked on steps 1-4

--- Models/ProfileContext.cs
+++ Models/ProfileContext.cs
@@ -1,10 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Net.Http.Headers;
+
 namespace MSWebPROJ2.Models;
 public class ProfileContext : DbContext
     {
+      public ProfileContext() { }
 
         public ProfileContext(DbContextOptions<ProfileContext> options) : base(options)
-        {
-        }
+        {}
         public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                ID = 1,
+                FirstName = "Kylan",
+                LastName = "Larson"
+            },
+            new User
+            {
+                ID = 2,
+                FirstName = "Nicholas",
+                LastName = "Woerner"
+            },
+            new User
+            {
+                ID = 3,
+                FirstName = "Michael",
+                LastName = "Myers"
+            });
     }
+}
--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -6,7 +6,9 @@ namespace MSWebPROJ2.Models
     {
         [Key]
         public int? ID { get; set; }
+        [Required(ErrorMessage = "Please put your first name in.")]
         public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Please put your last name in.")]
         public string? LastName { get; set; }
         
     }
